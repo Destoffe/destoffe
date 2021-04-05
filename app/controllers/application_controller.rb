c43bf79f154bea1fc4f0f5b88c603d492180869e
Destoffe <destoffe@hotmail.se>
2021-04-05 21:02:47 +0200

    asd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
-	cert = LetsEncrypt::Certificate.create(domain: 'destoffe.se')
-	cert.get
+  cert = LetsEncrypt::Certificate.create(domain: 'destoffe.se')
+  cert.get
+
+  cert = LetsEncrypt::Certificate.find_by(domain: 'destoffe.se')
+  cert.verify
+
+  cert = LetsEncrypt::Certificate.find_by(domain: 'destoffe..se')
+  cert.issue
 end
