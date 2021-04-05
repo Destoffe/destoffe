21350003a2b5d870e37a69865fbd3f69c02d47c6
Destoffe <destoffe@hotmail.se>
2021-04-05 21:38:06 +0200

    asd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
 
+
+cert = LetsEncrypt::Certificate.create(domain: 'destoffe.se')
+cert.get # alias  `verify && issue`
+
+cert = LetsEncrypt::Certificate.find_by(domain: 'destoffe.se')
+cert.verify
+
+cert = LetsEncrypt::Certificate.find_by(domain: 'destoffe.se')
+cert.issue
+
+cert = LetsEncrypt::Certificate.find_by(domain: 'destoffe.se')
+cert.renew
 end
